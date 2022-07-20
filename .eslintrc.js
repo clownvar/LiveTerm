94e708fcfb8023da1894aa7783dd8a5353c9c849
BERCHIL Idriss <16048625+Jonahgay@users.noreply.github.com>
2022-07-20 11:11:52 +0100

    Please enter the commit message for your changes. Lines starting

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,8 @@ module.exports = {
     'no-console': 'error',
     'prettier/prettier': 'warn',
     'react-hooks/exhaustive-deps': 'off',
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
   },
+  
 };
