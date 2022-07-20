b62c6c54248921036753d9cb439fc54914b9e85d
clownvar <16048625+Jonahgay@users.noreply.github.com>
2022-07-20 13:31:39 +0100

    Edit sumfetch

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -4,7 +4,7 @@ import * as bin from './index';
 import config from '../../../config.json';
 
 // Help
-export const help = async (args: string[]): Promise<string> => {
+export const aide = async (args: string[]): Promise<string> => {
   const commands = Object.keys(bin).sort().join(', ');
   var c = '';
   for (let i = 1; i <= Object.keys(bin).sort().length; i++) {
@@ -14,11 +14,11 @@ export const help = async (args: string[]): Promise<string> => {
       c += Object.keys(bin).sort()[i - 1] + ' ';
     }
   }
-  return `Welcome! Here are all the available commands:
+  return `Bienvenu! Voici toutes les commandes disponibles:
 \n${c}\n
-[tab]: trigger completion.
-[ctrl+l]/clear: clear terminal.\n
-Type 'sumfetch' to display summary.
+[tab]: déclancher la completion.
+[ctrl+l]/clear: vider la console.\n
+Tapez 'sumfetch' pour afficher le résumé.
 `;
 };
 
@@ -29,18 +29,19 @@ Type 'sumfetch' to display summary.
 };*/
 
 // About
-export const about = async (args: string[]): Promise<string> => {
-  return `Hi, I am ${config.name}. 
-Welcome to my website!
-More about me:
-'sumfetch' - short summary.
-'resume' - my latest resume.
-'readme' - my github readme.`;
+export const apropos = async (args: string[]): Promise<string> => {
+  return `Salut, Je suis ${config.name}.
+Bienvenue sur mon site web!
+
+Plus à propos de moi:
+'sumfetch' - résumé court.
+'CV' - mon dernier CV.
+'readme' - mon readme github.`;
 };
 
-export const resume = async (args: string[]): Promise<string> => {
+export const CV = async (args: string[]): Promise<string> => {
   window.open(`${config.resume_url}`);
-  return 'Opening resume...';
+  return 'Ouverture du  curriculum vitae...';
 };
 
 // Donate
@@ -138,26 +139,20 @@ export const sudo = async (args?: string[]): Promise<string> => {
 
 // Banner
 export const banner = (args?: string[]): string => {
-  return `
-  ⠀⠀⠀⠀⢀⣤⡄⢠⣤⣤⠀⣠⣤⠄⠀⠀⠀⠀⠀⠀⢄⣀⣀⠀⠀⠀⠀⠀⠀⠤⡤⢄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-  ⠀⣠⡴⢚⡿⠿⡿⠛⢁⣼⣼⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣅⣠⣴⣶⡖⠛⠲⠄⠀⠘⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-  ⢰⠛⢀⡼⠀⠀⠀⠀⣼⢏⣿⢁⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⠈⢿⣳⠂⠀⠈⠓⠄⠀⠀⠀⠈⢺⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-  ⢸⠀⢸⣇⠀⠀⢀⢴⣿⣼⠃⠊⠀⠀⢀⣤⡶⣫⣥⣄⡀⠀⠀⢀⠛⢯⣁⣒⡢⢄⠀⢀⠀⠀⠘⣏⡇⠀⣀⣀⠀⠀⠀⠀⠀⠀⠀
-  ⠘⢇⡄⠙⢲⡖⢁⣾⣿⠋⠀⠀⢠⠀⠋⠁⣾⡞⣩⣭⡽⠀⠀⠀⣰⠋⠀⢀⣹⣼⠀⠀⢣⠀⠀⢻⢳⣾⣩⡌⠉⠲⡀⠀⠀⠀⠀
-  ⠀⠀⠻⣶⠧⣶⣿⡟⠁⣀⡤⢄⡀⠀⠀⠀⠘⠃⠽⢿⡇⠀⠀⢠⣿⡆⠀⣾⣿⣏⠀⠀⢸⠀⠀⠀⠁⢀⢿⡿⠀⠀⠹⡦⣄⡀⠀
-  ⠀⠀⠀⠀⠉⠉⠻⡇⣼⢋⣀⣀⠻⡆⠀⠀⠀⠀⠀⠈⠀⠀⠀⠈⢻⣿⣆⠀⠀⠉⠀⣠⠎⠀⣠⣀⠤⠚⠀⠁⠀⠀⣰⣿⣿⠳⣄
-  ⠀⠀⠀⠀⠀⠀⠀⠹⣏⣿⠷⣿⣓⣻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣻⣶⡶⢖⣚⡋⠀⠀⠈⠉⠀⠀⠀⣠⣤⣴⣭⣛⠇⠀⠈
-  ⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⡁⠿⠏⢸⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣷⣄⣀⣈⡀⠐⠒⢾⣦⣀⠾⠟⠀⠀⢹⣿⢧⢶⣾
-  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣷⡀⠀⠈⡷⢤⠀⠀⠀⠀⣀⡀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⣽⣿⣿⣄⠀⠀⠀⠁⠨⠊⠈⢩
-  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣧⠀⠀⠁⠀⠀⢀⠴⢋⡠⠭⣶⣤⣀⠹⢿⣿⡿⠛⠟⠋⠉⠻⣿⣿⣿⣿⣿⣷⣦⣤⣤⣤⢤⣶⠋
-  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⡄⠀⠀⢰⡁⣔⣡⣴⣿⣿⠟⣿⠀⡰⠋⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀
-  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣶⣄⠀⠙⢿⣿⠛⢛⣡⡾⣻⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠛⠉⠀⠀⠀
-  ⠀⠀⠀⢀⣠⣶⠒⢶⣄⠀⣸⢻⡽⠒⢻⣿⢿⣦⣄⠙⣿⣛⣩⡼⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-  ⠀⠀⢀⡞⠋⠹⣧⣄⡼⠞⢱⠋⠀⢠⢿⡟⠃⠙⣿⣿⣾⣍⠉⢰⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-  ⠀⠀⢸⣧⡀⠀⠐⠢⡆⠀⡇⠀⡀⠸⣮⡳⠄⠀⠙⢻⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-  ⠀⠀⠀⠛⠛⠒⠒⠂⠒⠊⠙⠒⠚⠂⠉⠁⠀⠀⠀⠀⠉⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  return String.raw`
+  __/\\\\\\\\\\\\\_______________________________________________/\\\________________/\\\\\\____        
+  _\/\\\/////////\\\____________________________________________\/\\\_______________\////\\\____       
+   _\/\\\_______\/\\\____________________________________________\/\\\__________/\\\____\/\\\____      
+    _\/\\\\\\\\\\\\\\______/\\\\\\\\___/\\/\\\\\\\______/\\\\\\\\_\/\\\_________\///_____\/\\\____     
+     _\/\\\/////////\\\___/\\\/////\\\_\/\\\/////\\\___/\\\//////__\/\\\\\\\\\\___/\\\____\/\\\____    
+      _\/\\\_______\/\\\__/\\\\\\\\\\\__\/\\\___\///___/\\\_________\/\\\/////\\\_\/\\\____\/\\\____   
+       _\/\\\_______\/\\\_\//\\///////___\/\\\_________\//\\\________\/\\\___\/\\\_\/\\\____\/\\\____  
+        _\/\\\\\\\\\\\\\/___\//\\\\\\\\\\_\/\\\__________\///\\\\\\\\_\/\\\___\/\\\_\/\\\__/\\\\\\\\\_ 
+         _\/////////////______\//////////__\///_____________\////////__\///____\///__\///__\/////////__
 
-Type 'help' to see the list of available commands.
-Type 'sumfetch' to display summary.
+Salut, Je suis ${config.name} 
+Bienvenue sur mon site web!
+Tapez 'aide' pour voir la liste des commandes disponibles.
+Tapez 'sumfetch' pour afficher le résumé.
 `;
 };
